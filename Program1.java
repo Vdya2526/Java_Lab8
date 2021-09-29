04087735fb83bdf0c605cfaf54b22dea5a502ec6
Vidyashree <91537412+Vdya2526@users.noreply.github.com>
2021-09-29 15:06:34 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,20 @@
+package lab8;
+
+import java.util.Scanner;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		
+		        String str1="vidya";
+		        String str2="vidya";
+		        
+		        // Comparing two input string
+		        if (str1.equals(str2))
+		            System.out.print("Equal Strings");
+		        else
+		            System.out.print("UnEqual Strings");
+
+	}
+
+}
---
+++ Program2.java
@@ -0,0 +1,10 @@
+package lab8;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
